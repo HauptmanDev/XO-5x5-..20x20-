071693dbae057128ef69956314e8506c0ffd6912
HauptmanDev <MainVados@gmail.com>
2020-03-18 23:30:52 +0300

    Fixed bags. Added style components.

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,8 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /(node_modules)/,
-                loader: "babel-loader",
+                exclude: /node_modules/,
+                use: ["babel-loader"]
             },
             {
                 test: /\.css$/,
